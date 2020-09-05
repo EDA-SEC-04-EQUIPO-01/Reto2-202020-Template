dcb322f4d9aa2bdde5ab5f80d1a44ab4c69b44d2
Kevin-C-S <kevincohensolano@gmail.com>
2020-09-05 17:47:53 -0500

    Segunda versión del catalog (model)

--- App/model.py
+++ App/model.py
@@ -36,58 +36,24 @@ es decir contiene los modelos con los datos en memoria
 
 def newCatalogMovies():
     
-    catalog = {'id': None,
-               'budget': None,
-               'genres': None,
-               'imdb_id': None,
-               'original_language': None,
-               'original_title': None,
-               'overview': None,
-               'popularity': None,
-               'production_companies': None,
-               'production_countries': None,
-               'release_date': None,
-               'revenue': None,
-               'runtime': None,
-               'spoken_languages': None,
-               'status': None,
-               'tagline': None,
-               'title': None,
-               'vote_average': None,
-               'vote_count': None,
-               'production_companies_number': None,
-               'production_countries_number': None,
-               'spoken_languages_number': None}
+    catalog = {'id': None, #Va
+               'genres': None, #Va
+               'original_title': None, #Va
+               'production_companies': None, #Va
+               'production_countries': None, #Va
+               'release_date': None, #Va
+               'vote_average': None, #Va
+               'vote_count': None} #Va
 
     catalog['id'] = lt.newList('SINGLE_LINKED', compare)
-    catalog['budget'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
     catalog['genres'] = mp.newMap(200,
                                    maptype='PROBING',
                                    loadfactor=0.4,
                                    comparefunction=compare)
-    catalog['imdb_id'] = mp.newMap(1000,
-                                maptype='CHAINING',
-                                loadfactor=0.7,
-                                comparefunction=compare)
-    catalog['original_language'] = mp.newMap(1000,
-                                  maptype='CHAINING',
-                                  loadfactor=0.7,
-                                  comparefunction=compare)
     catalog['original_title'] = mp.newMap(500,
                                  maptype='CHAINING',
                                  loadfactor=0.7,
                                  comparefunction=compare)
-    catalog['overview'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['popularity'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
     catalog['production_companies'] = mp.newMap(1000,
                                 maptype='CHAINING',
                                 loadfactor=0.7,
@@ -100,30 +66,6 @@ def newCatalogMovies():
                                  maptype='CHAINING',
                                  loadfactor=0.7,
                                  comparefunction=compare)
-    catalog['revenue'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['runtime'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['spoken_languages'] = mp.newMap(1000,
-                                maptype='CHAINING',
-                                loadfactor=0.7,
-                                comparefunction=compare)
-    catalog['status'] = mp.newMap(1000,
-                                  maptype='CHAINING',
-                                  loadfactor=0.7,
-                                  comparefunction=compare)
-    catalog['tagline'] = mp.newMap(500,
-                                 maptype='CHAINING',
-                                 loadfactor=0.7,
-                                 comparefunction=compare)
-    catalog['title'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
     catalog['vote_average'] = mp.newMap(200,
                                    maptype='PROBING',
                                    loadfactor=0.4,
@@ -132,49 +74,21 @@ def newCatalogMovies():
                                 maptype='CHAINING',
                                 loadfactor=0.7,
                                 comparefunction=compare)
-    catalog['production_companies_number'] = mp.newMap(1000,
-                                  maptype='CHAINING',
-                                  loadfactor=0.7,
-                                  comparefunction=compare)
-    catalog['production_countries_number'] = mp.newMap(500,
-                                 maptype='CHAINING',
-                                 loadfactor=0.7,
-                                 comparefunction=compare)
-    catalog['spoken_languages_number'] = mp.newMap(500,
-                                 maptype='CHAINING',
-                                 loadfactor=0.7,
-                                 comparefunction=compare)
 
     return catalog
 
 def newCatalogCasting():
 
-    catalog = {'id': None,
-               'actor1_name': None,
-               'actor1_gender': None,
-               'actor2_name': None,
-               'actor2_gender': None,
-               'actor3_name': None,
-               'actor3_gender': None,
-               'actor4_name': None,
-               'actor4_gender': None,
-               'actor5_name': None,
-               'actor5_gender': None,
-               'actor_number': None,
-               'director_name': None,
-               'director_gender': None,
-               'director_number': None,
-               'producer_name': None,
-               'producer_number': None,
-               'screeplay_name': None,
-               'editor_name': None}
+    catalog = {'id': None, #Va
+               'actor1_name': None, #Va
+               'actor2_name': None, #Va
+               'actor3_name': None, #Va
+               'actor4_name': None, #Va
+               'actor5_name': None, #Va
+               'director_name': None} #Va
 
     catalog['id'] = lt.newList('SINGLE_LINKED', compare)
     catalog['actor1_name'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['actor1_gender'] = mp.newMap(200,
                                    maptype='PROBING',
                                    loadfactor=0.4,
                                    comparefunction=compare)
@@ -182,66 +96,22 @@ def newCatalogCasting():
                                 maptype='CHAINING',
                                 loadfactor=0.7,
                                 comparefunction=compare)
-    catalog['actor2_gender'] = mp.newMap(1000,
-                                  maptype='CHAINING',
-                                  loadfactor=0.7,
-                                  comparefunction=compare)
     catalog['actor3_name'] = mp.newMap(500,
                                  maptype='CHAINING',
                                  loadfactor=0.7,
                                  comparefunction=compare)
-    catalog['actor3_gender'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
     catalog['actor4_name'] = mp.newMap(200,
                                    maptype='PROBING',
                                    loadfactor=0.4,
                                    comparefunction=compare)
-    catalog['actor4_gender'] = mp.newMap(1000,
-                                maptype='CHAINING',
-                                loadfactor=0.7,
-                                comparefunction=compare)
     catalog['actor5_name'] = mp.newMap(1000,
                                   maptype='CHAINING',
                                   loadfactor=0.7,
                                   comparefunction=compare)
-    catalog['actor5_gender'] = mp.newMap(500,
-                                 maptype='CHAINING',
-                                 loadfactor=0.7,
-                                 comparefunction=compare)
-    catalog['actor_number'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
     catalog['director_name'] = mp.newMap(200,
                                    maptype='PROBING',
                                    loadfactor=0.4,
                                    comparefunction=compare)
-    catalog['director_gender'] = mp.newMap(1000,
-                                maptype='CHAINING',
-                                loadfactor=0.7,
-                                comparefunction=compare)
-    catalog['director_number'] = mp.newMap(1000,
-                                  maptype='CHAINING',
-                                  loadfactor=0.7,
-                                  comparefunction=compare)
-    catalog['producer_name'] = mp.newMap(500,
-                                 maptype='CHAINING',
-                                 loadfactor=0.7,
-                                 comparefunction=compare)
-    catalog['producer_number'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['screeplay_name'] = mp.newMap(200,
-                                   maptype='PROBING',
-                                   loadfactor=0.4,
-                                   comparefunction=compare)
-    catalog['editor_name'] = mp.newMap(1000,
-                                maptype='CHAINING',
-                                loadfactor=0.7,
-                                comparefunction=compare)
 
     return catalog
 
